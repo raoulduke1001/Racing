f0dcdbfc5e97222040c500ba7e95dd0de265b4c5
raoulduke1001 <1raoulduke1@gmail.com>
2020-01-15 00:23:15 +0300

    0.2

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const score = document.querySelector('.score'),
     start = document.querySelector('.start'),
     gameArea = document.querySelector('.gameArea'),
     car = document.createElement('div');
-    car.classList.add('car');
+car.classList.add('car');
 
 start.addEventListener('click', startGame);
 document.addEventListener('keydown', startRun);
@@ -18,20 +18,47 @@ const keys = {
 const setting = {
     start: false,
     score: 0,
-    speed: 0
+    speed: 3
 };
 
 function startGame() {
     start.classList.add('hide');
+    for (let i = 0; i < 20; i++) {
+        const line = document.createElement('div');
+        line.classList.add('line');
+        line.style.top = (i * 100) + 'px';
+        line.y = i * 100;
+        gameArea.appendChild(line);
+    }
+
     setting.start = true;
     gameArea.appendChild(car);
+    setting.x = car.offsetLeft;
+    setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
 }
 
 function playGame() {
-    if (setting.start){
-    requestAnimationFrame(playGame);
-}
+    moveRoad();
+    if (setting.start) {
+        if (keys.ArrowLeft && setting.x > 0) {
+            setting.x -= setting.speed;
+        }
+        if (keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)) {
+            setting.x += setting.speed;
+        }
+
+        if (keys.ArrowDown && setting.y < (gameArea.offsetHeight - car.offsetHeight)) {
+            setting.y += setting.speed;
+        }
+        if (keys.ArrowUp && setting.y > 0) {
+            setting.y -= setting.speed;
+        }
+
+        car.style.left = setting.x + "px";
+        car.style.top = setting.y + "px";
+        requestAnimationFrame(playGame);
+    }
 }
 
 function startRun(event) {
@@ -42,4 +69,16 @@ function startRun(event) {
 function stopRun(event) {
     event.preventDefault();
     keys[event.key] = false;
+}
+
+function moveRoad() {
+    let lines = document.querySelectorAll(".line");
+    lines.forEach(function (line) {
+        line.y += setting.speed;
+        line.style.top = line.y + "px";
+        if (line.y >= document.documentElement.clientHeight) {
+            line.y = -100;
+        }
+
+    });
 }
