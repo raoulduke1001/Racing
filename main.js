76f46d71084a2e2262a859de1170daf72d9011a7
raoulduke1001 <1raoulduke1@gmail.com>
2020-01-15 15:25:15 +0300

    0.3

--- main.js
+++ main.js
@@ -18,17 +18,33 @@ const keys = {
 const setting = {
     start: false,
     score: 0,
-    speed: 3
+    speed: 3,
+    traffic: 3
 };
+
+function getQuantityElements(heightElement) {
+    return document.documentElement.clientHeight / heightElement + 1;
+
+}
+
 
 function startGame() {
     start.classList.add('hide');
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < getQuantityElements(100); i++) {
         const line = document.createElement('div');
         line.classList.add('line');
         line.style.top = (i * 100) + 'px';
         line.y = i * 100;
         gameArea.appendChild(line);
+    }
+    for (let i = 0; i < getQuantityElements(100 * setting.traffic); i++) {
+        const enemy = document.createElement('div');
+        enemy.classList.add('enemy');
+        enemy.y = -100 * setting.traffic * (i + 1);
+        enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + "px";
+        enemy.style.top = enemy.y + 'px';
+        enemy.style.background = 'transparent url(./img/enemy.png) center / cover no-repeat;';
+        gameArea.appendChild(enemy);
     }
 
     setting.start = true;
@@ -40,6 +56,7 @@ function startGame() {
 
 function playGame() {
     moveRoad();
+    moveEnemy();
     if (setting.start) {
         if (keys.ArrowLeft && setting.x > 0) {
             setting.x -= setting.speed;
@@ -81,4 +98,20 @@ function moveRoad() {
         }
 
     });
+}
+
+function moveEnemy() {
+    let enemy = document.querySelectorAll('.enemy');
+    enemy.forEach(function (item) {
+        item.y += setting.speed / 2;
+        item.style.top = item.y + 'px';
+        if (item.y >= document.documentElement.clientHeight) {
+            item.y = -100 * setting.traffic;
+            item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + "px";
+
+        }
+
+    });
+
+
 }
